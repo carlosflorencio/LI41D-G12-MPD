5d7582256da74410bd607cee26e30d2b39ac5865
Carlos Florencio <carlosmflorencio@gmail.com>
2015-04-15 11:56:52 +0100

    Removed files

--- src/main/java/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by lfalcao on 04/03/2015.
- */
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
--- src/main/java/isel/mpd/weather/weatherApp.md
+++
@@ -1,9 +0,0 @@
-# Weather Application
-
-## Main Problems to solve
-
-* Data reading
- * Data StreamProvider
- * Data Parsing 
-* Data filtering
-* Display results
